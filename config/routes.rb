b2895eef96b71c4523d1fc3271e1f2c2f8c4dcba
rebuilt <memoryman51@hotmail.com>
2018-07-14 15:50:50 +0200

    added stylesheet, changed image reference in stylesheet

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get 'home/index'
+
+  get 'styles/atoms'
+
+  get 'styles/molecules'
+
+  get 'styles/organisms'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
