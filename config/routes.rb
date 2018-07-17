560ae29a923e257416bf82f869983729ff9add5a
rebuilt <memoryman51@hotmail.com>
2018-07-17 21:05:29 +0200

    added root link in header

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  root to: 'home#index'
+
   get 'home/index'
 
   get 'styles/atoms'
@@ -9,8 +12,6 @@ Rails.application.routes.draw do
 
   get 'ideas/index'
 
-
-  root to: 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
