a9a80ea094355ff0575eaa3425c6faa7dffa9f47
rebuilt <memoryman51@hotmail.com>
2018-07-17 22:16:53 +0200

    added form helper

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,6 @@ Rails.application.routes.draw do
 
   get 'ideas/index'
 
-  get '/', to: 'home#index'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
