7b8d9dec8f7f44d01c89dabe8f445de7464bc287
rebuilt <memoryman51@hotmail.com>
2018-07-17 21:40:01 +0200

    replaced static root link with helper method

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
 
   get 'ideas/index'
 
+  get '/', to: 'home#index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
