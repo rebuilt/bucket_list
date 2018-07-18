d313ec86ae4623d9bddcb4a4ebda325db0443c0d
rebuilt <memoryman51@hotmail.com>
2018-07-18 08:02:14 +0200

    added routes to ideas#new and ideas#create

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
 
   get 'ideas/index'
 
+  get 'ideas/new'
+
+  post 'ideas/create'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
