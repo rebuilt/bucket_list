d13d1140a931f608c2b0fe7c5b9ebddf34a6c490
rebuilt <memoryman51@hotmail.com>
2018-07-18 08:24:46 +0200

    redirect ideas#create to ideas#index

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -4,8 +4,9 @@ class IdeasController < ApplicationController
     logger.info("The search term is: #{@search_term}")
   end
   def new
-
+    
   end
   def create
+    redirect_to ideas_index_path
   end
 end
