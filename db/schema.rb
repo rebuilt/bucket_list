481815e2c0f614b2093f058c326e903bef649882
rebuilt <memoryman51@hotmail.com>
2018-07-18 23:37:34 +0200

    ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180718065130) do
+ActiveRecord::Schema.define(version: 20180718213525) do
 
   create_table "ideas", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "done_count"
   end
 
 end
