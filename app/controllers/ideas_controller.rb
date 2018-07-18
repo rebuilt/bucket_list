71491728a3bc9c1057b666aca667965be50d762a
rebuilt <memoryman51@hotmail.com>
2018-07-18 07:59:53 +0200

    added 'new' and 'create' actions to controller

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -3,4 +3,9 @@ class IdeasController < ApplicationController
     @search_term = params[:q]
     logger.info("The search term is: #{@search_term}")
   end
+  def new
+
+  end
+  def create
+  end
 end
