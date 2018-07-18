c93932ad3ef07b7845b807dbad379b28595c9b48
rebuilt <memoryman51@hotmail.com>
2018-07-18 10:13:11 +0200

    created reference to idea model in controller

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -2,6 +2,7 @@ class IdeasController < ApplicationController
   def index
     @search_term = params[:q]
     logger.info("The search term is: #{@search_term}")
+    @ideas = Idea.all
   end
   def new
     
