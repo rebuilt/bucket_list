77352cb5c0154e1096f334a31acf64fd0bb189f3
rebuilt <memoryman51@hotmail.com>
2018-07-18 20:45:38 +0200

    generated controller for account, added get rout for /account/ideas/, populated default html for ideas.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,10 @@ Rails.application.routes.draw do
 
   get 'ideas/new'
 
+  get 'account/ideas'
+
   post 'ideas/create'
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
