7f3e174ab71069322433f8854174e4cf78b12677
rebuilt <memoryman51@hotmail.com>
2018-07-20 22:26:51 +0200

    confirmed test url includes styles/atoms, and that the page includes Atoms, Molecules

--- test/system/simple_navigations_test.rb
+++ test/system/simple_navigations_test.rb
@@ -9,14 +9,18 @@ class SimpleNavigationsTest < ApplicationSystemTestCase
   test 'search term is displayed' do
     visit('/')
     assert page.has_content?('Bucket List')
-    # fill_in('q', with: 'Spain').send_keys(:enter)
-    # assert has_content?('Spain')
-    # assert current_url.include?('q=Spain')
+     fill_in('q', with: 'Spain').send_keys(:enter)
+     assert has_content?('Spain')
+     assert current_url.include?('q=Spain')
   end
 
   test 'style guide navigation' do
-    # visit('/')
-    # click_on('Styleguide')
-    # assert current_url.include?('/styles/index')
+     visit('/')
+     click_on('Style Guide')
+     sleep(1.second)
+     assert current_url.include?('/styles/atoms')
+     sleep(8.second)
+     assert has_content?('Atoms')
+     assert has_content?('Molecules')
   end
 end
