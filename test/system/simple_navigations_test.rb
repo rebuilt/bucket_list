26d780ad1c22fe4aea16bb23b3516b6a5ffd9703
rebuilt <memoryman51@hotmail.com>
2018-07-20 15:33:49 +0200

    created new test

---
+++ test/system/simple_navigations_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class SimpleNavigationsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit simple_navigations_url
+  #
+  #   assert_selector "h1", text: "SimpleNavigation"
+  # end
+end
