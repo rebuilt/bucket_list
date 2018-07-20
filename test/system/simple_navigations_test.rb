80e5347add6282c2ca0cfb2333b71e6a7426599d
rebuilt <memoryman51@hotmail.com>
2018-07-20 16:13:16 +0200

    populated first test

--- test/system/simple_navigations_test.rb
+++ test/system/simple_navigations_test.rb
@@ -6,4 +6,11 @@ class SimpleNavigationsTest < ApplicationSystemTestCase
   #
   #   assert_selector "h1", text: "SimpleNavigation"
   # end
+  test 'search term is displayed' do
+    visit('/')
+    assert page.has_content?('Bucket List')
+    fill_in('q', with: 'Spain').send_keys(:enter)
+    assert has_content?('Spain')
+    assert current_url.include?('q=Spain')
+  end
 end
