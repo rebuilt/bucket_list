7d923fce37421ecfe0e0897099946b83527f8e8b
rebuilt <memoryman51@hotmail.com>
2018-07-24 00:06:40 +0200

    test inserts two records into our database and confirms they are loaded

--- test/system/ideas_test.rb
+++ test/system/ideas_test.rb
@@ -11,9 +11,33 @@ class IdeasTest < ApplicationSystemTestCase
     visit ideas_new_path
     fill_in 'title', with: 'See the matterhorn'
     fill_in 'done_count', with: "3"
+    fill_in 'photo_url', with: "https://cdn.shopify.com/s/files/1/0871/3066/products/FJ-1019-CappuccinoCup190ml-Feijoa-Cropped_1024x1024.jpg?v=1524591768"
+    sleep(10.seconds)
     click_on 'Update Profile'
     visit ideas_index_path
     assert page.has_content?('See the matterhorn')
+    sleep(10.seconds)
+
+  end
+
+  test 'create two ideasi records' do
+    idea1 = Idea.new
+    idea1.title = "Cycle across Australia"
+    idea1.done_count = 2
+    idea1.photo_url = "https://cdn.shopify.com/s/files/1/0871/3066/products/FJ-1019-CappuccinoCup190ml-Feijoa-Cropped_1024x1024.jpg?v=1524591768"
+    idea1.save!
+
+    idea2 = Idea.new
+    idea2.title = "Road rage championship"
+    idea2.done_count = 2
+    idea2.photo_url = "https://i.ytimg.com/vi/qvE2miLMbNk/maxresdefault.jpg"
+    idea2.save!
+    
+    visit ideas_index_path 
+    assert page.has_content?("Cycle across Australia")
+    assert page.has_content?("Road rage championship")
+    assert_equal 4, page.all('.card').count
+    #sleep(10.seconds)
 
   end
 end
