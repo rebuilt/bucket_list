75e1616a24293c42e9f74b5fce31f3f2da4ce874
rebuilt <memoryman51@hotmail.com>
2018-07-27 21:39:57 +0200

    used image_tag to dynamically load user image

--- config/routes.rb
+++ config/routes.rb
@@ -23,9 +23,6 @@ Rails.application.routes.draw do
   get 'ideas/:id', to: 'ideas#show' , as: 'show_idea'
 
   get 'account/ideas'
- 
-
-
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/system/show_ideas_test.rb
+++ test/system/show_ideas_test.rb
@@ -5,10 +5,14 @@ class ShowIdeasTest < ApplicationSystemTestCase
   test 'show displays title' do
     idea = Idea.new
     idea.title = "See the northern lights"
+    idea.done_count = 3
     idea.photo_url =  "http://fpoimg.com/255x170"
     idea.save!
 
     visit(show_idea_path(idea))
     assert page.has_content?('See the northern lights')
+    assert page.has_content?("#{3} have done this")
+    click_on 'Edit'
+
   end
 end
