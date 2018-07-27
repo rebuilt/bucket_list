da40efc20c14cab1b875691d6ab482bda64faf90
rebuilt <memoryman51@hotmail.com>
2018-07-27 21:51:18 +0200

    created_at timestamp appears for Idea page.  Test also clicks edit button

--- test/system/show_ideas_test.rb
+++ test/system/show_ideas_test.rb
@@ -12,7 +12,10 @@ class ShowIdeasTest < ApplicationSystemTestCase
     visit(show_idea_path(idea))
     assert page.has_content?('See the northern lights')
     assert page.has_content?("#{3} have done this")
+    assert page.has_content?("Created on #{idea.created_at.strftime("%d %b '%y'")}")
+    # sleep(10.seconds)
     click_on 'Edit'
+
 
   end
 end
