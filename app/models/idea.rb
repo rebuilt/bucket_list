3ca2ee7e47e88a958f825d10858f53be4ddb866e
rebuilt <memoryman51@hotmail.com>
2018-09-26 21:51:05 +0200

    completes association definition for comments

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,6 +1,9 @@
 class Idea < ApplicationRecord
   validates :title, presence: true
   validates :title, length: {maximum:75}
+
+  has_many :comments
+
   def self.search(search_term)
     where('title LIKE  ?', "%#{search_term}%")
   end
