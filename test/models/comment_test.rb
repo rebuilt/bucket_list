439a1245a1015e7d95bd620faf72b1ff23a917da
rebuilt <memoryman51@hotmail.com>
2018-09-26 21:49:57 +0200

    test changing the associated idea for a comment

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -4,4 +4,19 @@ class CommentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test 'changing the associated Idea for a comment' do
+    idea = Idea.new(title: 'Riding a camel')
+    idea.save!
+    comment = Comment.new
+    comment.body = "I'd like to do this"
+    comment.idea = idea
+    comment.save!
+
+    idea2 = Idea.new(title: 'dancing in the dark')
+    idea2.save!
+    comment.idea = idea2
+    comment.save!
+
+    assert_equal idea2, Comment.first.idea 
+  end
 end
