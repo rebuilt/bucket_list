a373a473d316890f0b0b3f0021b0672adbbc16fc
rebuilt <memoryman51@hotmail.com>
2018-09-26 22:01:16 +0200

    test comments are ordered correctly'

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -19,4 +19,33 @@ class CommentTest < ActiveSupport::TestCase
 
     assert_equal idea2, Comment.first.idea 
   end
+
+  test 'cascading save' do
+    idea = Idea.new(title: 'Riding a camel')
+    idea.save!
+   
+    comment = Comment.new(body: 'Great idea!') 
+    idea.comments  << comment
+    idea.save!
+
+    assert_equal comment, Comment.first
+  end
+
+  test 'Comments are ordered correctly' do
+
+    idea = Idea.new(title: 'Riding a camel')
+    idea.save!
+   
+    comment = Comment.new(body: 'That would be great fun') 
+    comment2 = Comment.new(body: 'I agree!  That sounds like a good time')
+    
+    idea.comments << comment
+    idea.comments << comment2
+    idea.save!
+
+    assert_equal idea.comments.first, comment
+    assert_equal 2, idea.comments.length
+
+
+  end
 end
