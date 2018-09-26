56eafed207ed01f7dae7fadee69e36cdaec1d6cc
rebuilt <memoryman51@hotmail.com>
2018-09-26 20:21:14 +0200

    if update errors, don't update database.  Instead, reload page with saved parameters.

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -28,10 +28,12 @@ class IdeasController < ApplicationController
   end
  
   def update
-    idea = Idea.find(params[:id])
-    idea.update(idea_resource_params)
-    idea.save!
+    @idea = Idea.find(params[:id])
+    if(@idea.update(idea_resource_params))
     redirect_to account_ideas_path
+    else
+      render 'edit'
+    end
   end
 
   private
