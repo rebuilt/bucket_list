b464d4c51ecdd4646678287c07f461b9790e9263
rebuilt <memoryman51@hotmail.com>
2018-10-11 10:30:58 +0200

    System test for adding a comment

---
+++ test/system/idea_comments_test.rb
@@ -0,0 +1,18 @@
+require "application_system_test_case"
+
+class IdeaCommentsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit idea_comments_url
+  #
+  #   assert_selector "h1", text: "IdeaComment"
+  # end
+  test 'adding a comment to and idea' do 
+    idea = Idea.new
+    idea.save
+    visit(idea_path(idea))
+    fill_in('Add a comment', with: 'This is a fantastic idea!')
+    click_on('Post', match: :first)
+    assert_equal idea_path(idea), page.current_path
+
+  end
+end
