11629a29db8af3ede09b0de687e6f518939fec20
rebuilt <memoryman51@hotmail.com>
2018-10-16 21:45:02 +0200

    created associations between ideas and users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181013102010) do
+ActiveRecord::Schema.define(version: 20181016193434) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20181013102010) do
     t.integer "done_count"
     t.string "photo_url"
     t.string "description"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_ideas_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
