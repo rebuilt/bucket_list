030b6620d6c82742ea9061ae62eb69eba74ecfb7
rebuilt <memoryman51@hotmail.com>
2018-10-16 21:30:18 +0200

    added log in tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,17 @@
 class UsersController < ApplicationController
-  def create
-
-  end
-  
   def new 
     @user = User.new
+  end
 
+  def create
+    user = User.find_or_create_by(user_params)
+    redirect_to ideas_path
+  end
+  
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email)
   end
 end
