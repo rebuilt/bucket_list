07cce2f15a909456b5f3f3824eead98df1506764
rebuilt <memoryman51@hotmail.com>
2019-01-28 21:25:07 +0100

    implemented create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to account_ideas_path
+    else
+      flash[:alert] = 'Email or password were invalid.  Please try again'
+      render 'new'
+    end
   end
 end
