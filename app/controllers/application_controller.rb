81494c7c87d4e897740cb5da3df88788d8f5598e
rebuilt <memoryman51@hotmail.com>
2019-01-29 20:56:45 +0100

    Added header method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :logged_in?
+  helper_method :logged_in?, :header
 
   def logged_in?
     session[:user_id].present?
+  end
+
+  def header
+    if logged_in?
+      render partial: 'authenticated_user_header'
+    else
+      render partial: 'anonymous_user_header'
+    end
   end
 end
