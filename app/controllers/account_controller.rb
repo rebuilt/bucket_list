b8524ef248419026be6f9d08363184cd085295a1
rebuilt <memoryman51@hotmail.com>
2019-01-30 20:36:44 +0100

    added update method and strict parameters

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -7,12 +7,11 @@ class AccountController < ApplicationController
     @ideas = user.ideas
   end
 
-  def edit
-
-  end
+  def edit; end
 
   def update
-
+    current_user.update(user_params)
+    redirect_to account_path
   end
 
   def ensure_authenticated
@@ -21,5 +20,11 @@ class AccountController < ApplicationController
 
   def current_user
     User.find(session[:user_id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :name, :avatar_url)
   end
 end
