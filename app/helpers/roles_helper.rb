658f5f0199f26e7b661cedd96d3b6a7df738cecf
rebuilt <memoryman51@hotmail.com>
2019-02-01 23:37:35 +0100

    added roles helper module with can_edit? helper method

---
+++ app/helpers/roles_helper.rb
@@ -0,0 +1,9 @@
+module RolesHelper 
+  def can_edit?(goal)
+    case current_user.role
+    when 'admin' then true
+    when 'registered' then current_user == goal.user
+    else false
+    end
+  end
+end
