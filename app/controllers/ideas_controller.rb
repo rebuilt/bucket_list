6c4778ee4db66e44aed952187e82d2e2f52c6897
rebuilt <memoryman51@hotmail.com>
2019-02-01 18:51:50 +0100

    moved instanciation of @idea to method, refactored code, added a before method for loading idea

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,4 +1,8 @@
 class IdeasController < ApplicationController
+  before_action :ensure_authenticated, only: %i[edit update]
+  before_action :load_idea,            only: %i[edit update]
+  before_action :ensure_owner,         only: %i[edit update]
+
   def index
     @search_term = params[:q]
     logger.info("The search term is: #{@search_term}")
@@ -6,7 +10,6 @@ class IdeasController < ApplicationController
   end
 
   def show
-    @idea = Idea.find(params[:id])
     @comment = Comment.new
     @display_add_comment = session[:user_id].present?
 
@@ -33,13 +36,9 @@ class IdeasController < ApplicationController
     end
   end
 
-  def edit
-    id = params[:id]
-    @idea = Idea.find(id)
-  end
+  def edit; end
 
   def update
-    @idea = Idea.find(params[:id])
     if @idea.update(idea_resource_params)
       redirect_to account_ideas_path
     else
@@ -50,6 +49,20 @@ class IdeasController < ApplicationController
   private
 
   def idea_resource_params
-    params.require(:idea).permit(:title, :description, :photo_url, :done_count, :name_of_user)
+    params.require(:idea).permit(:title,
+                                 :description,
+                                 :photo_url,
+                                 :done_count,
+                                 :name_of_user)
+  end
+
+  def load_idea
+    @idea = Idea.find(params[:id])
+  end
+
+  def ensure_owner
+    return if idea.user == current_user
+
+    redirect_to(account_path)
   end
 end
