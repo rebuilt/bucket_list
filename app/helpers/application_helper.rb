de9d9f3d5c682a67b44db949a083501dd800774a
rebuilt <memoryman51@hotmail.com>
2019-02-01 23:39:18 +0100

    moved can_edit? function to RolesHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,2 @@
 module ApplicationHelper
-  def can_edit?(goal)
-    case current_user.role
-    when 'admin' then true
-    when 'registered' then current_user == goal.user
-    else false
-    end
-  end
 end
