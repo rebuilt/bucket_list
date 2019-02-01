5531766217120df4038bff6fc04c966ef5751ad5
rebuilt <memoryman51@hotmail.com>
2019-02-01 23:04:59 +0100

    instead of sending additional server query, use current_user method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,12 @@
 class CommentsController < ApplicationController
+  before_action :ensure_authenticated,  only: %i[create]
+  helper_method :current_user
+
   def create
-    user = User.find(session[:user_id])
     comment = Comment.new comment_params
     idea = Idea.find(params[:idea_id])
     comment.idea = idea
-    comment.user = user
+    comment.user = current_user 
     comment.save
 
     redirect_to idea_path(idea)
