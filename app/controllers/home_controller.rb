7e82dc1f79ef450aa6e00e911c803fc97d3646ce
rebuilt <memoryman51@hotmail.com>
2019-02-01 18:50:25 +0100

    added instance variable for user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
     @ideas = Idea.most_recent
+    if session[:user_id].present?
+      @user = User.find(session[:user_id])
+    else
+      @user = nil
+    end
   end
 end
