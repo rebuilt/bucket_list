413e4f557ea1bdb4f3e6d58aabc0797c31acbfce
rebuilt <memoryman51@hotmail.com>
2019-02-01 18:49:04 +0100

    moved methods from other controllers for reuse

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  helper_method :logged_in?, :header
+  helper_method :logged_in?, :header, :current_user
 
   def logged_in?
     session[:user_id].present?
@@ -13,5 +13,17 @@ class ApplicationController < ActionController::Base
     else
       render partial: 'anonymous_user_header'
     end
+  end
+
+  def current_user
+    return @current_user if @current_user.present?
+
+    @current_user = User.find(session[:user_id])
+  end
+
+  private
+
+  def ensure_authenticated
+    redirect_to login_path unless logged_in?
   end
 end
