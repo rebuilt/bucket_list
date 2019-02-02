96bbba3b0e361fdb3b2c64a9740ec3c000e4734d
rebuilt <memoryman51@hotmail.com>
2019-02-02 09:39:06 +0100

    defined default role and enforce inclusion of either "registered" or "admin" as role

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,18 @@ class User < ApplicationRecord
   has_and_belongs_to_many :goals, class_name: 'Idea'
   has_secure_password
   before_validation :downcase_email
+  after_initialize :default_role!
+  validates :role, inclusion: { in: %w(registered admin) }
   validates :email, uniqueness: true
+
 
   private
 
   def downcase_email
     self.email = email.downcase unless self.email.nil?
+  end
+
+  def default_role!
+    self.role ||= 'registered'
   end
 end
