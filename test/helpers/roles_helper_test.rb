95479619b8e3b3482307578fcfa202c24e1cf29a
rebuilt <memoryman51@hotmail.com>
2019-02-02 12:03:36 +0100

    added tests for admins and users

--- test/helpers/roles_helper_test.rb
+++ test/helpers/roles_helper_test.rb
@@ -11,4 +11,21 @@ class RolesHelperTest < ActionView::TestCase
 
     assert can_edit?(idea)
   end
+
+  test 'admin can edit any idea' do
+    user = User.create(email: 'admin@epfl.ch', role: 'admin')
+    self.current_user = user
+    idea = Idea.create(title: 'A test idea', user: User.create(email: 'alt@epfl.ch'))
+    assert can_edit?(idea)
+  end
+
+  test 'registered user cannot edit ideas he doesnt own' do
+    user = User.create(email: 'test@epfl.ch')
+    self.current_user = user
+
+    idea = Idea.create(title: 'A test idea', user: User.create(email: 'alt@epfl.ch'))
+
+    assert_not can_edit?(idea)
+  end
+
 end
