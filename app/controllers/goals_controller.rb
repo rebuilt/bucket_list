4b768a2f1d5a3cc9971ab880b5197f41f3a07149
rebuilt <memoryman51@hotmail.com>
2019-02-13 09:55:55 +0100

    do not add goal if its already on the list

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -3,7 +3,7 @@ class GoalsController < ApplicationController
 
   def create
     idea = Idea.find(params[:idea_id])
-    current_user.goals << idea
+    current_user.goals << idea unless current_user.goals.include?(idea)
     redirect_to account_goals_path
   end
 end
