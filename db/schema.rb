b679f6ee337a997fae0f5470648cd9c018444d2d
rebuilt <memoryman51@hotmail.com>
2019-02-16 11:55:25 +0100

    configure carrierwave uploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190201184534) do
+ActiveRecord::Schema.define(version: 20190216104426) do
 
   create_table "comments", force: :cascade do |t|
     t.string "body"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20190201184534) do
     t.string "name"
     t.string "avatar_url"
     t.string "role"
+    t.string "avatar"
   end
 
 end
