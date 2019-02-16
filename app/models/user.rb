93b6677dc3454929b6d649bb560e951e5dcd3da4
rebuilt <memoryman51@hotmail.com>
2019-02-16 13:44:41 +0100

    formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,17 @@
 class User < ApplicationRecord
   has_many :comments
   has_many :ideas
+
   has_and_belongs_to_many :goals, class_name: 'Idea'
+
   has_secure_password
+
   before_validation :downcase_email
   after_initialize :default_role!
+
   validates :role, inclusion: { in: %w[registered admin] }
   validates :email, uniqueness: true
+
   mount_uploader :avatar, AvatarUploader
 
   private
