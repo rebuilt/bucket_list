c637bca544d9e3c345ac6efeacf73d831345a820
rebuilt <memoryman51@hotmail.com>
2019-02-17 22:50:02 +0100

    create startup script for production

---
+++ run-production.sh
@@ -0,0 +1,3 @@
+export SECRET_KEY_BASE=309a8e0ec7baf5cae855fa87c115738f83d3de49fc3d541297a760cf3e9809e677ffd8bd5f7d871a06731bc44a310af730ad3b35a156a9c832ba5dc74f3226d2
+export RAILS_SERVE_STATIC_FILES=true
+rails server --environment production
